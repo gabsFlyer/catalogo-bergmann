fa1276e1df6f71a6011e4f3a0136cb2fb1e1cb48
Gabriel Monteiro <gmonteeeiro@gmail.com>
2022-05-28 15:10:17 -0300

    Cria lógica básica para o sistema de autenticação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './modules/dashboard/components/dashboard/dashboard.component';
+import { FlyerComponent } from './modules/flyer/components/flyer/flyer.component';
+import { LoginComponent } from './modules/login/components/login/login.component';
+import { AuthGuard } from './shared/guards/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'catalogo',
+    pathMatch: 'full'
+  },
+  {
+    path: 'catalogo',
+    pathMatch: 'full',
+    component: FlyerComponent
+  },
+  {
+    path: 'login',
+    pathMatch: 'full',
+    component: LoginComponent
+  },
+  {
+    path: 'dashboard',
+    pathMatch: 'full',
+    component: DashboardComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: '**',
+    redirectTo: 'login'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule { }
---
+++ src/app/modules/flyer/flyer.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FlyerComponent } from './components/flyer/flyer.component';
+
+
+
+@NgModule({
+  declarations: [
+    FlyerComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+})
+export class FlyerModule { }
