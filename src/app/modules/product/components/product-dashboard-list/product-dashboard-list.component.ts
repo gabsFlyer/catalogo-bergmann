70e3e60cd4e04b0dd5e82ba3c4d50e74ad67f18f
Gabriel Monteiro <gmonteeeiro@gmail.com>
2022-06-05 00:38:07 -0300

    Monta produtos na tela

--- src/app/modules/product/components/product-dashboard-list/product-dashboard-list.component.ts
+++ src/app/modules/product/components/product-dashboard-list/product-dashboard-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { IResponsePageable } from 'src/app/shared/interfaces/response-pageable.interface';
+import { Product } from 'src/app/shared/models/product.model';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-product-dashboard-list',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDashboardListComponent implements OnInit {
 
-  constructor() { }
+  currentPage: number = 1;
+  totalItems: number = 0;
+  itemsPerPage: number = 10;
+  products: Array<Product> = new Array();
+
+  constructor(
+    private productService: ProductService,
+  ) { }
 
   ngOnInit(): void {
+    this.loadProducts();
+  }
+
+  loadProducts() {
+    this.productService.getProducts(this.currentPage)
+      .subscribe((products: IResponsePageable<Array<Product>>) => {
+        this.products = products.data;
+        this.itemsPerPage = products.per_page;
+        this.totalItems = products.total;
+      });
+  }
+
+  pageChange(page: number) {
+    this.currentPage = page;
+    this.loadProducts();
   }
 
 }
--- src/app/modules/product/product.module.ts
+++ src/app/modules/product/product.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductDashboardListComponent } from './components/product-dashboard-list/product-dashboard-list.component';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 
 
@@ -10,6 +11,7 @@ import { ProductDashboardListComponent } from './components/product-dashboard-li
   ],
   imports: [
     CommonModule,
+    NgxPaginationModule,
   ],
   exports: [
     ProductDashboardListComponent,
