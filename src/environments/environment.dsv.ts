7a2937f657e0f0f5936bdc5b92c6c088b77bc978
Gabriel Monteiro <gmonteeeiro@gmail.com>
2022-06-17 17:52:43 -0300

    Cria environment para development

---
+++ src/environments/environment.dsv.ts
@@ -0,0 +1,11 @@
+const apiUrl = '';
+
+export const environment = {
+  production: false,
+  sessionStorage: {
+    userToken: 'catalogo-user-token',
+  },
+  api: {
+    endpoint: `${apiUrl}/api`,
+  }
+};
