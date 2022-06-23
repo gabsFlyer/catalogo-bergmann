91add289d40840739e385008bb08043731298abb
Gabriel Monteiro <gmonteeeiro@gmail.com>
2022-06-23 20:50:32 -0300

    Adiciona empresa nas contantes

--- src/app/shared/constants/routes.constant.ts
+++ src/app/shared/constants/routes.constant.ts
@@ -10,6 +10,11 @@ export const RoutesConstant = {
       list: 'dashboard/products',
       edit: 'dashboard/products/edit/{0}',
       new:  'dashboard/products/new'
-    }
+    },
+    enterprises: {
+      list: 'dashboard/enterprises',
+      edit: 'dashboard/enterprises/edit/{0}',
+      new:  'dashboard/enterprises/new'
+    },
   }
 }
