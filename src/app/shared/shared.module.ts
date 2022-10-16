df9868912c07feb8bb945edea1d1d6efb42ff4ed
Gabriel Monteiro <gmonteeeiro@gmail.com>
2022-10-16 13:43:37 -0300

    Ajusta navbar para funcionar com usuário logado ou não

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,7 +2,7 @@ import { UnauthorizedInterceptor } from './interceptors/unauthorized.interceptor
 import { TokenInterceptor } from './interceptors/token.interceptor';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavbarDashboardComponent } from './components/navbar-dashboard/navbar-dashboard.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ImageUploadComponent } from './components/image-upload/image-upload.component';
 import { MatIconModule } from '@angular/material/icon';
@@ -11,7 +11,7 @@ import { UnprocessableEntityInterceptor } from './interceptors/unprocessable-ent
 
 @NgModule({
   declarations: [
-    NavbarDashboardComponent,
+    NavbarComponent,
     ImageUploadComponent,
   ],
   imports: [
@@ -36,7 +36,7 @@ import { UnprocessableEntityInterceptor } from './interceptors/unprocessable-ent
     }
   ],
   exports: [
-    NavbarDashboardComponent,
+    NavbarComponent,
     ImageUploadComponent,
   ]
 })
